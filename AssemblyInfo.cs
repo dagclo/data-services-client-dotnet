91862c8dab358cc60a87e61ab4b0405f79a3220c
Andrew Miller <amiller@satorisoftware.com>
2018-05-11 14:45:49 -0700

    Bumping version
    - downgrading dependency versions

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.1.*")]
 [assembly: AssemblyCompany("Quadient Data USA")]
 [assembly: AssemblyProduct("Quadient Data Services Client")]
 [assembly: AssemblyTitle("Quadient Data Services Client")]
