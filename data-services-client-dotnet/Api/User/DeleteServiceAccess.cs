479068d37a94bbd66a7e4b8798d166f5d93495e9
Shawn Kao <s.kao@quadient.com>
2018-05-21 17:18:54 -0700

    added comments

--- data-services-client-dotnet/Api/User/DeleteServiceAccess.cs
+++ data-services-client-dotnet/Api/User/DeleteServiceAccess.cs
@@ -3,6 +3,9 @@ using System.Net.Http;
 
 namespace Quadient.DataServices.Api.User
 {
+    /// <summary>
+    /// Remove the service access descriptor for the named service.
+    /// </summary>
     public class DeleteServiceAccess : IRequest<object, object>
     {
         public string ServicePath { get; }
@@ -10,6 +13,11 @@ namespace Quadient.DataServices.Api.User
         public object Content { get; set; }
         public IDictionary<string, string> QueryStringParams { get; }
 
+        /// <summary>
+        /// Remove the service access descriptor for the named service.
+        /// </summary>
+        /// <param name="tenantId">A unique tenant id. Tenant ids/hrefs can be discovered via enumeration/querying (GET /users/v1/tenants).</param>
+        /// <param name="serviceName">A unique service name. Service names can be discovered via enumeration/querying (GET /services)</param>
         public DeleteServiceAccess(string tenantId, string serviceName)
         {
             ServicePath = $"/users/v1/tenants/{tenantId}/{serviceName}";
