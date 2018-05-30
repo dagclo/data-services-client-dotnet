fe9c607441e492c35ea2ffa363d5194d941bf1b8
Shawn Kao <s.kao@quadient.com>
2018-05-30 14:06:40 -0700

    added requests for adding and retrieving tenant

---
+++ data-services-client-system/User/GetTenant.cs
@@ -0,0 +1,20 @@
+﻿using Quadient.DataServices.Api;
+using Quadient.DataServices.System.Model.User;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Quadient.DataServices.System.User
+{
+    public class GetTenant : IRequest<object, TenantInformation>
+    {
+        public string ServicePath { get; }
+        public HttpMethod Method { get; } = HttpMethod.Get;
+        public object Content { get; set; }
+        public IDictionary<string, string> QueryStringParams { get; }
+
+        public GetTenant(string tenantName)
+        {
+            ServicePath = $"users/v1/tenants/{tenantName}";
+        }
+    }
+}
