20a85f46dba43f993c07b425e46b838d50864a0a
Shawn Kao <s.kao@quadient.com>
2018-08-08 14:12:42 -0700

    added request to get ws job

---
+++ data-services-client-dotnet/Api/WalkSequence/GetWalkSequenceJob.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Quadient.DataServices.Api.WalkSequence
+{
+	public class GetWalkSequenceJob : IRequest<object, DataServies.Model.WalkSequence.Job>
+	{
+		public string ServicePath { get; }
+
+		public HttpMethod Method { get; } = HttpMethod.Get;
+
+		public object Content { get; set; }
+
+		public IDictionary<string, string> QueryStringParams { get; }
+
+		public GetWalkSequenceJob(string jobId)
+		{
+			ServicePath = $"services/walk-sequence/v1/jobs/{jobId}";
+		}
+	}
+}
