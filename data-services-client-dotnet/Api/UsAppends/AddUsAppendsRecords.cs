95accfda8861dc6da8252dd313365da35c24d2b7
Shawn Kao <s.kao@quadient.com>
2018-09-18 18:09:37 -0700

    fix adding records

--- data-services-client-dotnet/Api/UsAppends/AddUsAppendsRecords.cs
+++ data-services-client-dotnet/Api/UsAppends/AddUsAppendsRecords.cs
@@ -6,12 +6,13 @@ namespace Quadient.DataServices.Api.UsBatch
 {
     public class AddUsAppendsRecords : IRequest<Records, object>
     {
-        public AddUsAppendsRecords(Records records)
+        public AddUsAppendsRecords(string jobId, List<List<string>> records)
         {
-            Content = records;
+            ServicePath = $"services/us-appends/v1/jobs/{jobId}/records";
+            Content = new Records {_Records = records};
         }
 
-        public string ServicePath { get; } = "services/us-appends/v1/records";
+        public string ServicePath { get; }
         public HttpMethod Method { get; } = HttpMethod.Post;
         public Records Content { get; set; }
         public IDictionary<string, string> QueryStringParams { get; }
