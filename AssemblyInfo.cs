31c1a18bb80ccc38a2f4fe6642669b966313c306
Shawn Kao <s.kao@quadient.com>
2019-01-08 09:46:38 -0800

    bump to v1.5.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
-[assembly: AssemblyVersion("1.4.0.*")]
+[assembly: AssemblyVersion("1.5.0.*")]
 [assembly: AssemblyCompany("Quadient Data USA")]
 [assembly: AssemblyProduct("Quadient Data Services Client")]
 [assembly: AssemblyTitle("Quadient Data Services Client")]
